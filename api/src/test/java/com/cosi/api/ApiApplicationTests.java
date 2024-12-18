32ce6ae3932987df3d23fd303f6614cdfca70bd1
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-18 16:30:07 +0900

    feat(/): 스프링 부트 프로젝트 생성
    프로젝트
    - 인텔리제이
    - Gradle-Groovy
    - Java 17
    - Spring Boot 3.4.0
    
    의존성
    - spring boot web
    - spring boot websocket
    - java_websocket 1.5.7
    - gson 2.11.0
    - lombok
    
    관련 이슈
    #1

---
+++ api/src/test/java/com/cosi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
