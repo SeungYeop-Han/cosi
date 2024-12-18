9707fddf1876f22cb2b25205b15e8d07b301b1bf
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-18 19:10:31 +0900

    test(CosiApplicationTests): RestClient 연동 테스트
    - RestClient 가 빈으로 잘 등록이 되었는지 확인한다.
    - 종목 리스트를 요청했을 때 Gzip 압축이 잘 해제되는 지 확인한다.
    - Assertions 방식을 사용하지 않고 그냥 눈으로 확인했다.
    
    관련 이슈
    #1

--- api/src/test/java/com/cosi/api/CosiApplicationTests.java
+++ api/src/test/java/com/cosi/api/CosiApplicationTests.java
@@ -1,13 +1,40 @@
 package com.cosi.api;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import net.minidev.json.parser.JSONParser;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestClient;
 
 @SpringBootTest
 class CosiApplicationTests {
+	
+	@Autowired
+	RestClient restClient;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void UpbitClientConfig에_등록된_RestClient가_잘_동작하는_지_눈으로_확인() {
+
+		// given
+		String uriString = "https://crix-static.upbit.com/crix_master";
+
+		// when
+		String responseBodyAsString = restClient
+				.get()
+				.uri(uriString)
+				.retrieve()
+				.body(String.class);
+
+		// then
+		System.out.println(" >>> " + responseBodyAsString);
+	}
 }
