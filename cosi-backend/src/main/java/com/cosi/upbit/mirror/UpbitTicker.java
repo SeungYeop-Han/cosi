953e9732e88ba990e1fffddc84b42b0a5f4947b8
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-20 19:11:48 +0900

    feat(UpbitTicker): 단일 종목 정보를 반환하는 메서드 추가
    getStatisticsOf: 해당하는 종목의 통계 정보를 반환
    getRealtimeQuotesOf: 해당하는 종목의 실시간 시세 정보를 반환
    
    관련 이슈
    #2

--- cosi-backend/src/main/java/com/cosi/upbit/mirror/UpbitTicker.java
+++ cosi-backend/src/main/java/com/cosi/upbit/mirror/UpbitTicker.java
@@ -3,6 +3,7 @@ package com.cosi.upbit.mirror;
 import com.cosi.upbit.dto.TickerRealtimeQuotes;
 import com.cosi.upbit.dto.TickerStatistics;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * 업비트에서 거래 중인 종목의 시세 정보를 제공합니다.
@@ -18,10 +19,24 @@ public interface UpbitTicker {
     Map<String, TickerStatistics> getStatistics();
 
     /**
+     * @param baseCurrencyCode
+     * @param quoteCurrencyCode
+     * @return 해당하는 종목의 통계 정보를 반환합니다. 만약 해당하는 종목이 없다면, 비어있는 Optional 을 반환합니다.
+     */
+    Optional<TickerStatistics> getStatisticsOf(String baseCurrencyCode, String quoteCurrencyCode);
+
+    /**
      * @return 전 종목의 실시간 시세 정보를 저장하는 Map 을 반환합니다. <br><br>
      * 이 Map 은 종목의 id 를 키로, 실시간 시세 정보를 값으로 가집니다. <br><br>
      * 여기서 실시간 시세는 가장 최근에 일어난 거래에 의해 결정된 현재가 또는 직전 현재가와 비교했을 때와의 차이 등을 말합니다.
      * 이 값은 수시로 변하기 때문에 실시간으로 업데이트 되어야 됩니다.
      */
     Map<String, TickerRealtimeQuotes> getRealtimeQuotes();
+
+    /**
+     * @param baseCurrencyCode
+     * @param quoteCurrencyCode
+     * @return 해당하는 종목의 실시간 시세 정보를 반환합니다. 만약 해당하는 종목이 없다면, 비어있는 Optional 을 반환합니다.
+     */
+    Optional<TickerRealtimeQuotes> getRealtimeQuotesOf(String baseCurrencyCode, String quoteCurrencyCode);
 }
