bb92983a1e88ca8cc137abdcf19461c6ee37f267
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-20 11:20:04 +0900

    docs(UpbitClientConfig): restClient() 빈 팩토리 메서드에 java doc 주석 추가
    관련 이슈
    #1

--- api/src/main/java/com/cosi/upbit/config/UpbitClientConfig.java
+++ api/src/main/java/com/cosi/upbit/config/UpbitClientConfig.java
@@ -10,6 +10,14 @@ import org.springframework.web.util.DefaultUriBuilderFactory.EncodingMode;
 @Configuration
 public class UpbitClientConfig {
 
+    /**
+     * 업비트 RestClient 빈 등록
+     * <ul>
+     *     <li>경로 변수 값에 슬래시가 포함되는 경우가 있으므로, uriBuilderFactory 를 EncodingMode.NONE 으로 설정한다.
+     *     <li>gzip 응답을 해제하는 인터셉터를 등록한다.
+     * </ul>
+     * @return 비동기 HTTP 통신을 위해 사용할 스프링 RestClient 빈 객체
+     */
     @Bean
     public RestClient restClient() {
 
