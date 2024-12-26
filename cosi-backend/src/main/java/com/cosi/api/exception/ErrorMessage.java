62644b83041957fa442950d63426362ef04678eb
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-26 18:42:36 +0900

    fix(ErrorMessage): public getter 메서드 추가
    언마샬링 할 대상 클래스에 public getter 가 없으면 No acceptable representation 에러가 발생합니다.
    
    관련 이슈
    #15

--- cosi-backend/src/main/java/com/cosi/api/exception/ErrorMessage.java
+++ cosi-backend/src/main/java/com/cosi/api/exception/ErrorMessage.java
@@ -1,10 +1,12 @@
 package com.cosi.api.exception;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * 오류 응답의 본문으로 언마샬링됩니다.
  */
+@Getter
 @AllArgsConstructor
 public class ErrorMessage {
     private int statusCode;
