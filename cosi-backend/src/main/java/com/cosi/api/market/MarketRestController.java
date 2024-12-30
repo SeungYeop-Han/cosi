26a1a9088a22fd5b1916a5c497ca5488ab395d9e
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-30 20:33:48 +0900

    feat(MarketRestController): 단일 종목 시세 정보(스냅샷) API 구현
    - UpbitTicker 빈 생성자 주입
    - 특정 종목에 대한 시세 정보 스냅샷을 반환하는 getMarketQuotes 메서드 정의
      - 찾지 못한 경우 BadRequestException 을 반환합니다.
    
    관련 이슈
    #10

--- cosi-backend/src/main/java/com/cosi/api/market/MarketRestController.java
+++ cosi-backend/src/main/java/com/cosi/api/market/MarketRestController.java
@@ -1,13 +1,17 @@
 package com.cosi.api.market;
 
+import com.cosi.api.exception.BadRequestException;
+import com.cosi.upbit.dto.TickerQuotes;
 import com.cosi.upbit.mirror.UpbitMarkets;
-import java.io.IOException;
+import com.cosi.upbit.mirror.UpbitTicker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,14 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class MarketRestController {
 
     private final UpbitMarkets upbitMarkets;
+    private final UpbitTicker upbitTicker;
 
     @Autowired
-    public MarketRestController(UpbitMarkets upbitMarkets) {
+    public MarketRestController(UpbitMarkets upbitMarkets, UpbitTicker upbitTicker) {
         this.upbitMarkets = upbitMarkets;
+        this.upbitTicker = upbitTicker;
     }
 
     @GetMapping
-    public ResponseEntity<byte[]> getMarkets() throws IOException {
+    public ResponseEntity<byte[]> getMarkets() {
         byte[] body = upbitMarkets.getGzipCompressedMarketListJson();
         return ResponseEntity
                 .ok()
@@ -32,5 +38,18 @@ public class MarketRestController {
                 .cacheControl(CacheControl.noCache())
                 .eTag(upbitMarkets.getEtag())
                 .body(body);
+    }
+
+    @GetMapping("/{marketCode}/quotes")
+    public ResponseEntity<TickerQuotes> getMarketQuotes(@PathVariable("marketCode") String marketCode) {
+        
+        TickerQuotes quotes = upbitTicker.getQuotesSnapshot(marketCode).orElseThrow(() -> {
+            // ToDo: BadRequestException 을 확장하는 특화 예외 클래스 정의 (ex. MarketNotFoundException)
+            throw new BadRequestException(HttpStatus.NOT_FOUND, marketCode + "에 해당하는 종목을 찾을 수 없습니다. 코드는 대문자로만 이루어져있습니다. 다시 한 번 확인해주세요.");
+        });
+
+        return ResponseEntity
+                .ok()
+                .body(quotes);
     }
 }
