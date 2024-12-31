60ab3b490747091d178a7a733807076cb6f9cc90
SeungYeop-Han <ammezkhan@gmail.com>
2024-12-31 16:15:37 +0900

    feat(MarketRestController): 단일 종목 통계량 정보(스냅샷) API 구현
    getMarketStatistics 핸들러는 UpbitTicker 빈에서 특정 빈의 통계량 정보 획득 후 반환합니다.
    
    경로 변수로써 전달받은 종목 코드를 찾을 수 없는 경우 예외를 발생시킵니다.
    
    관련 이슈
    #10

--- cosi-backend/src/main/java/com/cosi/api/market/MarketRestController.java
+++ cosi-backend/src/main/java/com/cosi/api/market/MarketRestController.java
@@ -2,6 +2,7 @@ package com.cosi.api.market;
 
 import com.cosi.api.exception.BadRequestException;
 import com.cosi.upbit.dto.TickerQuotes;
+import com.cosi.upbit.dto.TickerStatistics;
 import com.cosi.upbit.mirror.UpbitMarkets;
 import com.cosi.upbit.mirror.UpbitTicker;
 import java.util.Map;
@@ -60,5 +61,18 @@ public class MarketRestController {
         return ResponseEntity
                 .ok()
                 .body(upbitTicker.getQuotes());
+    }
+
+    @GetMapping("/{marketCode}/statistics")
+    public ResponseEntity<TickerStatistics> getMarketStatistics(@PathVariable("marketCode") String marketCode) {
+
+        TickerStatistics statistics = upbitTicker.getStatisticsSnapshot(marketCode).orElseThrow(() -> {
+            // ToDo: BadRequestException 을 확장하는 특화 예외 클래스 정의 (ex. MarketNotFoundException)
+            throw new BadRequestException(HttpStatus.NOT_FOUND, marketCode + "에 해당하는 종목을 찾을 수 없습니다. 코드는 대문자로만 이루어져있습니다. 다시 한 번 확인해주세요.");
+        });
+
+        return ResponseEntity
+                .ok()
+                .body(statistics);
     }
 }
