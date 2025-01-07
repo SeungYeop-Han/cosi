d9dc3dba10fbd376b2a4a695e90a55d9018fadea
SeungYeop-Han <ammezkhan@gmail.com>
2025-01-07 16:25:55 +0900

    refactor: 종목 시세 및 통계량을 저장하는 Map 필드 분리
    종목 시세 및 통계량을 저장하는 Map 필드를 호가통화코드를 기준으로 분리한다.
    분리하는 이유는 특정 호가 통화에 대한 스냅샷 정보를 빠르게 전달해주기 위함이다.
    (분리하지 않으면, 특정 호가 통화로 거래하는 모든 종목을 매번 필터링 해야 한다)
    
    UpbitTicker 인터페이스
    - getStatistics 메서드와 getQuotes 메서드는 더 이상 전 종목에 대한
      Map 을 반환하지 않는다. 이를 나타내기 위해 두 메서드의 이름은 다음과 같이 변경한다.
      - getStatistics -> getStatisticsMapWhichQuoteCurrencyCodeIs
      - getQuotes -> getQuoetsMapWhichQuoteCurrencyCodeIs
    
    UpbitTickerImpl 구상 클래스
    - 인터페이스 메서드 명 변경을 반영한다.
    - 전 종목의 시세와 통계량 정보를 저장하는 Map 필드 `quotesMap` 과 `statisticsMap` 을
      호가코드 "KRW", "BTC", "USDT" 를 기준으로 분리한다.
      이에 따라 updateQuotes 및 updateStatistics 메서드도 변경한다.
    - 가독성을 위해 종목 코드로부터 호가통화코드를 추출하는 코드를
      extractQuoteCurrencyCode 메서드로 분리한다.
    
    관련 이슈
    #10

--- cosi-backend/src/main/java/com/cosi/upbit/mirror/UpbitTickerImpl.java
+++ cosi-backend/src/main/java/com/cosi/upbit/mirror/UpbitTickerImpl.java
@@ -11,15 +11,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class UpbitTickerImpl implements UpbitTicker {
 
-    private final Map<String, TickerQuotes> quotesMap = new HashMap<>();
-    private final Map<String, TickerStatistics> statisticsMap = new HashMap<>();
+    private final Map<String, Map<String, TickerQuotes>> mapOfQuotesMaps = Map.of(
+            "KRW", new HashMap<>(),
+            "BTC", new HashMap<>(),
+            "USDT", new HashMap<>()
+    );
+
+    private final Map<String, Map<String, TickerStatistics>> mapOfStatisticsMaps = Map.of(
+            "KRW", new HashMap<>(),
+            "BTC", new HashMap<>(),
+            "USDT", new HashMap<>()
+    );
 
     @Override
     public void updateQuotes(TickerQuotes tickerQuotes) {
         if (tickerQuotes == null) {
             throw new NullPointerException("tickerQuotes 가 null 입니다.");
         }
-        quotesMap.put(tickerQuotes.getCode(), tickerQuotes);
+
+        mapOfQuotesMaps
+                .get(extractQuoteCurrencyCode(tickerQuotes.getCode()))
+                .put(tickerQuotes.getCode(), tickerQuotes);
     }
 
     @Override
@@ -27,26 +39,53 @@ public class UpbitTickerImpl implements UpbitTicker {
         if (tickerStatistics == null) {
             throw new NullPointerException("tickerStatistics 가 null 입니다.");
         }
-        statisticsMap.put(tickerStatistics.getCode(), tickerStatistics);
+
+        mapOfStatisticsMaps
+                .get(extractQuoteCurrencyCode(tickerStatistics.getCode()))
+                .put(tickerStatistics.getCode(), tickerStatistics);
     }
 
     @Override
-    public Map<String, TickerStatistics> getStatistics() {
-        return Collections.unmodifiableMap(statisticsMap);
+    public Map<String, TickerStatistics> getStatisticsMapWhichQuoteCurrencyCodeIs(String quoteCurrencyCode) {
+        var found = mapOfStatisticsMaps.get(quoteCurrencyCode);
+        if (found == null) {
+            throw new NullPointerException("호가통화코드 " + quoteCurrencyCode + "에 대한 통계량 Map 을 찾을 수 없습니다.");
+        }
+        return Collections.unmodifiableMap(mapOfStatisticsMaps.get(quoteCurrencyCode));
     }
 
     @Override
     public Optional<TickerStatistics> getStatisticsSnapshot(String marketCode) {
-        return Optional.ofNullable(statisticsMap.get(marketCode));
+        return Optional.ofNullable(
+                mapOfStatisticsMaps
+                        .get(extractQuoteCurrencyCode(marketCode))
+                        .get(marketCode)
+        );
     }
 
     @Override
-    public Map<String, TickerQuotes> getQuotes() {
-        return Collections.unmodifiableMap(quotesMap);
+    public Map<String, TickerQuotes> getQuotesMapWhichQuoteCurrencyCodeIs(String quoteCurrencyCode) {
+        var found = mapOfQuotesMaps.get(quoteCurrencyCode);
+        if (found == null) {
+            throw new NullPointerException("호가통화코드 " + quoteCurrencyCode + "에 대한 시세 Map 을 찾을 수 없습니다.");
+        }
+        return null;
     }
 
     @Override
     public Optional<TickerQuotes> getQuotesSnapshot(String marketCode) {
-        return Optional.ofNullable(quotesMap.get(marketCode));
+        return Optional.ofNullable(
+                mapOfQuotesMaps
+                        .get(extractQuoteCurrencyCode(marketCode))
+                        .get(marketCode)
+        );
+    }
+
+    /**
+     * @param marketCode {호가통화코드}-{기준통화코드} 형식의 종목 코드 (ex. KRW-BTC)
+     * @return 호가통화코드를 반환한다.
+     */
+    private String extractQuoteCurrencyCode(String marketCode) {
+        return marketCode.substring(0, marketCode.indexOf('-'));
     }
 }
