a87e9718566b025a6ba4fae86530c012650e3e4c
SeungYeop-Han <ammezkhan@gmail.com>
2025-01-07 15:49:59 +0900

    refactor: 불필요한 API 제거
    다음 API 는 불필요합니다.
    - 전체 종목 시세 스냅샷
    - 단일 종목 시세 스냅샷
    - 전체 종목 24시간누적거래량 및 거래대금
    
    관련 이슈
    #10

--- cosi-backend/src/main/java/com/cosi/api/market/MarketRestController.java
+++ cosi-backend/src/main/java/com/cosi/api/market/MarketRestController.java
@@ -1,13 +1,9 @@
 package com.cosi.api.market;
 
 import com.cosi.api.exception.BadRequestException;
-import com.cosi.upbit.dto.TickerQuotes;
 import com.cosi.upbit.dto.TickerStatistics;
-import com.cosi.upbit.dto.TickerStatisticsView.Acc24HOnly;
 import com.cosi.upbit.mirror.UpbitMarkets;
 import com.cosi.upbit.mirror.UpbitTicker;
-import com.fasterxml.jackson.annotation.JsonView;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
@@ -44,27 +40,6 @@ public class MarketRestController {
                 .body(body);
     }
 
-    @GetMapping("/{marketCode}/quotes")
-    public ResponseEntity<TickerQuotes> getMarketQuotes(@PathVariable("marketCode") String marketCode) {
-        
-        TickerQuotes quotes = upbitTicker.getQuotesSnapshot(marketCode).orElseThrow(() -> {
-            // ToDo: BadRequestException 을 확장하는 특화 예외 클래스 정의 (ex. MarketNotFoundException)
-            throw new BadRequestException(HttpStatus.NOT_FOUND, marketCode + "에 해당하는 종목을 찾을 수 없습니다. 코드는 대문자로만 이루어져있습니다. 다시 한 번 확인해주세요.");
-        });
-
-        return ResponseEntity
-                .ok()
-                .body(quotes);
-    }
-
-    @GetMapping("/quotes")
-    public ResponseEntity<Map<String, TickerQuotes>> getMarketQuotes() {
-
-        return ResponseEntity
-                .ok()
-                .body(upbitTicker.getQuotes());
-    }
-
     @GetMapping("/{marketCode}/statistics")
     public ResponseEntity<TickerStatistics> getMarketStatistics(@PathVariable("marketCode") String marketCode) {
 
@@ -76,14 +51,5 @@ public class MarketRestController {
         return ResponseEntity
                 .ok()
                 .body(statistics);
-    }
-
-    @GetMapping("/statistics/acc24h")
-    @JsonView(Acc24HOnly.class)
-    public ResponseEntity<Map<String, TickerStatistics>> getMarketStatisticsAcc24HOnly() {
-
-        return ResponseEntity
-                .ok()
-                .body(upbitTicker.getStatistics());
     }
 }
